4671fb810a55d6c68a1b059943caf6fa9625d210
MckCieply <mwppl123@gmail.com>
2025-04-27 16:14:06 +0200

    Created event form

--- DateMate/app/src/main/java/com/mckcieply/datemate/ui/home/HomeFragment.kt
+++ DateMate/app/src/main/java/com/mckcieply/datemate/ui/home/HomeFragment.kt
@@ -4,17 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.mckcieply.datemate.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,17 +18,30 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.submitButton.setOnClickListener {
+            val title = binding.titleInput.text.toString()
+            val description = binding.descriptionInput.text.toString()
+            val location = binding.locationInput.text.toString()
+            val start = binding.startTimeInput.text.toString()
+            val end = binding.endTimeInput.text.toString()
+
+            val summary = """
+                Title: $title
+                Description: $description
+                Location: $location
+                From: $start
+                To: $end
+            """.trimIndent()
+
+            Toast.makeText(requireContext(), summary, Toast.LENGTH_LONG).show()
         }
-        return root
     }
 
     override fun onDestroyView() {
